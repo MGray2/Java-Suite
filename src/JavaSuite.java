84013953ca3a90ded3f845320570aa5ca620e94d
MGray2 <mgray24@basecampcodingacademy.org>
2024-02-13 14:17:57 -0600

    Initial Commit

---
+++ src/JavaSuite.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class JavaSuite {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Java Suite™");
+        frame.setSize(690, 420);
+        frame.setVisible(true);
+    }
+}
