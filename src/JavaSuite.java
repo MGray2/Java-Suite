3174ca0da9a10793a3bc7fba47ee0273fd475f9e
MGray2 <mgray24@basecampcodingacademy.org>
2024-02-13 14:56:24 -0600

    added natural dark mode and other things

--- src/JavaSuite.java
+++ src/JavaSuite.java
@@ -1,10 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
+import java.time.*;
 
 public class JavaSuite {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("Java Suite™");
-        frame.setSize(690, 420);
-        frame.setVisible(true);
+        frame.setSize(690, 420); // window size
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true); // needed to see window
+        ImageIcon logo = new ImageIcon("images/java.svg");
+        frame.setIconImage(logo.getImage());
+
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
+        LocalTime evening = LocalTime.parse("18:00:00");
+        LocalTime morning = LocalTime.parse("06:00:00");
+        if (currentTime.isAfter(evening) || currentTime.isBefore(morning)) {
+            frame.getContentPane().setBackground(Color.black);
+        } else {
+            frame.getContentPane().setBackground(Color.white);
+        }
+
     }
 }
