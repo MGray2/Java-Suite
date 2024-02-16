84661e6ff5629dd7f01e42646912e10579df917c
MGray2 <mgray24@basecampcodingacademy.org>
2024-02-16 09:23:07 -0600

    Expense Tracker Complete

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,53 +1,79 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Income {
+    String name; // name of the income
+    double value; // value of the income
+    Income(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        String valueFormatted = String.format("%.2f", value);
+        return name + ": $" + valueFormatted;
+    }
+
+}
+
 class Expense {
-    String expenseName;
-    double value;
-    Expense(String expenseName, double value) {
-        this.expenseName = expenseName;
+    String name; // name of the expense
+    double value; // price of the expense
+    Expense(String name, double value) {
+        this.name = name;
         this.value = value;
         // constructor method
     }
     @Override
     public String toString() {
         String valueFormatted = String.format("%.2f", value);
-        return expenseName + ": $" + valueFormatted;
+        return name + ": $" + valueFormatted;
     }
 
-    void modify(String newName, double newValue) {
-        expenseName = newName;
-        value = newValue;
-    }
 
-    void modify(String newName) {
-        expenseName = newName;
-    }
-
-    void modify(double newValue) {
-        value = newValue;
-    }
 }
 
 public class ExpenseTracker {
-    static ArrayList<Expense> Expenses = new ArrayList<>();
+    static ArrayList<Expense> Expenses = new ArrayList<>(); // list to hold expenses
+    static ArrayList<Income> IncomeList = new ArrayList<>(); // list to hold income
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             int n = 0;
-            for (Expense i : Expenses) { // this for loop prints each expense in list
+            double totalIncome = 0.0;
+            System.out.println("Income (" + IncomeList.size() + ")");
+            for (Income i : IncomeList) {
                 System.out.println(n + "# " + i.toString());
+                totalIncome += i.value;
                 n++;
             }
-            System.out.println("[A]dd expense, [R]emove expense, [C]hange expence, [Q]uit.");
+            String totalFormatted = String.format("%.2f", totalIncome);
+            System.out.println("Total: $" + totalFormatted);
+
+            n = 0;
+            double totalExpense = 0;
+            System.out.println("Expenses (" + Expenses.size() + ")");
+            for (Expense i : Expenses) { // this for loop prints each expense in list
+                System.out.println(n + "# " + i.toString());
+                totalExpense += i.value;
+                n++;
+            }
+            double total = totalIncome - totalExpense;
+            totalFormatted = String.format("%.2f", total);
+            System.out.println("Total: $" + totalFormatted);
+
+            System.out.println("[A]dd expense, [R]emove expense, [+] add income, [-] remove income, [Q]uit.");
+            String newName;
+            double newValue;
             switch (scanner.nextLine().toLowerCase()) {
                 case "a":
                     System.out.println("Name of expense:");
-                    String newName = scanner.nextLine();
+                    newName = scanner.nextLine();
                     System.out.println("Value of expense:");
-                    float newValue = scanner.nextFloat();
+                    newValue = scanner.nextFloat();
                     scanner.nextLine();
                     Expense newExpense = new Expense(newName, newValue); // creation of expense
                     Expenses.add(newExpense); // append to list
@@ -71,46 +97,38 @@ public class ExpenseTracker {
                         System.err.println("Index does not exist");
                         break;
                             }
-
-
                     scanner.nextLine();
-                case "c":
+
+                case "+":
+                    System.out.println("Name of income:");
+                    newName = scanner.nextLine();
+                    System.out.println("Value of income:");
+                    newValue = scanner.nextFloat();
+                    scanner.nextLine();
+                    Income newIncome = new Income(newName, newValue);
+                    IncomeList.add(newIncome); // add to list
+                    break;
+
+                case "-":
                     n = 0;
-                    for (Expense i : Expenses) {
+                    for (Income i : IncomeList) {
                         System.out.println(n + "# " + i.toString());
                         n++;
                     }
                     try {
-                    System.out.println("Please use index number to select item");
-                    int p = scanner.nextInt();
-                    scanner.nextLine();
+                        System.out.println("Please use index number to select item for deletion.");
 
-                    if (Expenses.get(p) != null) {
-                        Expense selected = Expenses.get(p);
-                        System.out.println("Provide new name for expense, leave empty to skip:");
-                        String rename = scanner.nextLine(); // rename
-
-                        System.out.println("*Optional* provide new value for expense, 0 to skip");
-                        double revalue = scanner.nextDouble(); // revalue
-                        scanner.nextLine();
-
-                        if (!rename.isEmpty() && revalue == 0.0) {
-                            selected.modify(rename);
-                        } else if (rename.isEmpty() && revalue != 0.0) {
-                            selected.modify(revalue);
-                        } else if (!rename.isEmpty()) {
-                            selected.modify(rename, revalue);
-                        } else {
-                            // nothing
+                        int m = scanner.nextInt();
+                        if (IncomeList.get(m) != null) {
+                            IncomeList.remove(m);
                             break;
                         }
-                        scanner.nextLine();
+                    }
+                    catch (IndexOutOfBoundsException e) {
+                        System.err.println("Index does not exist");
                         break;
                     }
 
-                    } catch (IndexOutOfBoundsException e) {
-                        System.err.println("Index does not exist");
-                    }
 
                 case "q":
                     System.exit(0);
